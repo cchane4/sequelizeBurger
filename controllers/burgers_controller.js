4d6272200e2f987a7131e923bfa4f07d7ce8528a
Chad Chaney <cchane4@Chads-MacBook-Air.local>
2017-02-03 18:48:03 -0600

    push 2

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,41 +1,39 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger");
+var burger = require("../models");
 
 // get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(data) {
-    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-    var hbsObject = { burgers: data };
-    res.render("index", hbsObject);
-  });
+     db.Burger.findAll({}).then(function(data) {
+        res.render("index", { burgers: data });
+    });
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+router.post("/", function(req, res) {
+   db.Burger.create({
+        burger_name: req.body.burger_name
+    }).then(function() {
+        res.redirect("/");
+    });
 });
 
+
 // put route -> back to index
-router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+router.put("/:id", function(req, res) {
+     db.Burger.update({
+        devoured: true
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(function() {
+        res.redirect("/");
+    });
 });
+
+  
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var app = express();
+var port = process.env.PORT || 3000;
+var db = require("./models");
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(process.cwd() + "/public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -19,12 +21,17 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+//app.use("/update", routes);
+//app.use("/create", routes);
 
 // listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port);
+//app.listen(port);
+//db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
